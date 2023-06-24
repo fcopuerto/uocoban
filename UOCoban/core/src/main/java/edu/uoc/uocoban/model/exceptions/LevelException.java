4afa3b0813743c6b0e6aa3181cb53fae3b047272
fcopuerto <fcopuerto@gmail.com>
2023-06-24 14:30:40 +0200

    first classes

---
+++ UOCoban/core/src/main/java/edu/uoc/uocoban/model/exceptions/LevelException.java
@@ -0,0 +1,17 @@
+package edu.uoc.uocoban.model.exceptions;
+
+import java.io.Serializable;
+
+public class LevelException extends Exception implements Serializable {
+
+    public static final String BOX_DESTINATION_ERROR = "[ERROR] There are not enough destinations for the boxes in the level";
+    public static final String BOX_ERROR = "[ERROR] There are no boxes in the level";
+    public static final String MAX_MOVEMENTS_ERROR = "[ERROR] The maximum number of movements should be greater than zero";
+    public static final String PARSING_LEVEL_FILE_ERROR = "[ERROR] There was some problem parsing the level file";
+    public static final String PLAYER_ERROR = "[ERROR] There must be one player for each level";
+    public static final String SIZE_ERROR = "[ERROR] There is a problem with the height/width of the level";
+
+    public LevelException(String message) {
+        super(message);
+    }
+}
---
+++ UOCoban/core/src/main/java/edu/uoc/uocoban/model/utils/Position.java
@@ -0,0 +1,5 @@
+package edu.uoc.uocoban.model.utils;
+
+public enum Position {
+    
+}
